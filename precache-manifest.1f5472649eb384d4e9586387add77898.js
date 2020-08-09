cc9eb6fff1c28344638ebf7f99925d63746bfd64
shashwatsahai <ssahai97@gmail.com>
2020-08-10 00:13:58 +0530

    Updates

---
+++ precache-manifest.1f5472649eb384d4e9586387add77898.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "60ea883dce86583b309aa10b99c20a09",
+    "url": "/orient-website/index.html"
+  },
+  {
+    "revision": "ea1478812ce2dcd9439b",
+    "url": "/orient-website/static/css/2.05d1d35e.chunk.css"
+  },
+  {
+    "revision": "83a38bcd04fb9cc96928",
+    "url": "/orient-website/static/css/main.03d6428a.chunk.css"
+  },
+  {
+    "revision": "ea1478812ce2dcd9439b",
+    "url": "/orient-website/static/js/2.8a50cd09.chunk.js"
+  },
+  {
+    "revision": "4d47ffde3dc69784e8e23fd910817ef8",
+    "url": "/orient-website/static/js/2.8a50cd09.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "83a38bcd04fb9cc96928",
+    "url": "/orient-website/static/js/main.419646ae.chunk.js"
+  },
+  {
+    "revision": "f0a5c0be228a906a0633",
+    "url": "/orient-website/static/js/runtime-main.1ec682a5.js"
+  },
+  {
+    "revision": "62e718de1e6673728d368a5e8888ab11",
+    "url": "/orient-website/static/media/preface.62e718de.png"
+  },
+  {
+    "revision": "b7c9e1e479de3b53f1e4e30ebac2403a",
+    "url": "/orient-website/static/media/slick.b7c9e1e4.woff"
+  },
+  {
+    "revision": "ced611daf7709cc778da928fec876475",
+    "url": "/orient-website/static/media/slick.ced611da.eot"
+  },
+  {
+    "revision": "d41f55a78e6f49a5512878df1737e58a",
+    "url": "/orient-website/static/media/slick.d41f55a7.ttf"
+  },
+  {
+    "revision": "f97e3bbf73254b0112091d0192f17aec",
+    "url": "/orient-website/static/media/slick.f97e3bbf.svg"
+  }
+]);
